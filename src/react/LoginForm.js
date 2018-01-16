cd093c187f3370ab84e248dcdb13dca49e285c24
Taras Mankovski <tarasm@gmail.com>
2018-01-16 16:23:09 -0500

    Added connect for React

--- src/react/LoginForm.js
+++ src/react/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function ReactLoginForm() {
+export default function ReactLoginForm(props) {
   return (
     <form className="log-in-form">
       <h4 className="text-center">Log in with you email account</h4>
@@ -13,7 +13,7 @@ export default function ReactLoginForm() {
         <input type="password" placeholder="Password" />
       </label>
       <input id="show-password" type="checkbox" />
-      <label for="show-password">Show password</label>
+      <label htmlFor="show-password">Show password</label>
       <p>
         <input type="submit" className="button expanded" value="Log in" />
       </p>
---
+++ src/react/connect.js
@@ -0,0 +1,53 @@
+import { Component, createElement } from 'react'
+import { map } from 'funcadelic'
+import hoistStatics from 'hoist-non-react-statics'
+import microstate from 'microstates'
+
+/**
+ * Connect function creates a wrapper around a React Component. This wrapper
+ * manages the microstate and ensuring that invoked transitions re-render the
+ * wrapped component.
+ *
+ * @param {Microstate} Model
+ * @param {ReactComponent} WrappedComponent
+ */
+export default function connect(Model, WrappedComponent) {
+  class Connect extends Component {
+    constructor(props, context) {
+      super(props, context)
+
+      /**
+       * Create microstate with initial state. This is what the users will see when
+       * the app first loads.
+       */
+      this.microstate = microstate(Model, {
+        ...props
+      })
+    }
+
+    /**
+     * Actions are transitions that are wrapped with code that'll cause the component to rerender.
+     */
+    get actions() {
+      return map(
+        transition => (...args) => {
+          this.microstate = transition(...args)
+          this.forceUpdate()
+        },
+        this.microstate
+      )
+    }
+
+    render() {
+      return createElement(WrappedComponent, {
+        model: this.microstate.state,
+        actions: this.actions,
+        ...this.props
+      })
+    }
+  }
+
+  Connect.WrappedComponent = WrappedComponent
+
+  return hoistStatics(Connect, WrappedComponent)
+}
